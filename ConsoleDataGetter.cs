e19e48144ec55c86cd7a732eab4f33e92a799ff4
AlyoshkinDV <DAleshkin@rpa-robin.ru>
2024-02-04 14:26:18 +0300

    полурабочий вариант с заготовками под рефакторинг

---
+++ ConsoleDataGetter.cs
@@ -0,0 +1,38 @@
+﻿namespace FileSignatureGeneration;
+
+public class ConsoleDataGetter
+{
+    public static string FilePath
+    {
+        get
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите путь к файлу");
+                var inputPath = Console.ReadLine();
+                if (!string.IsNullOrEmpty(inputPath) && File.Exists(inputPath))
+                    return inputPath;
+
+                Console.WriteLine("Введен некорректный или несуществующий путь к файлу");
+                Console.WriteLine();
+            }
+        }
+    }
+    
+    public static long SegmentSizeInBytes
+    {
+        get
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите размер сегмента в байтах");
+                var input = Console.ReadLine();
+                if (long.TryParse(input, out var segmentSize))
+                    return segmentSize;
+
+                Console.WriteLine("Введен некорректный размер сегмента");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SegmentGenerationData.cs
@@ -0,0 +1,19 @@
+﻿namespace FileSignatureGeneration;
+
+public class SegmentGenerationData
+{
+    public string FilePath { get; private set; }
+    
+    public long SegmentSizeInBytes { get; private set; }
+    
+    public Lazy<FileStream> FileStream => new(File.Open(FilePath, FileMode.Open));
+
+    public static SegmentGenerationData GetData()
+    {
+        return new SegmentGenerationData
+        {
+            FilePath = ConsoleDataGetter.FilePath,
+            SegmentSizeInBytes = ConsoleDataGetter.SegmentSizeInBytes
+        };
+    }
+}
