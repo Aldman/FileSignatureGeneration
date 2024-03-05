9e7ac7b034e1915cde8744898bab3ad3312d96d2
AlyoshkinDV <DAleshkin@rpa-robin.ru>
2024-03-05 23:00:41 +0300

    Рабочий вариант, но нужен рефакторинг

--- SegmentGenerator.cs
+++ SegmentGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 using FileSignatureGeneration.Utils;
@@ -15,6 +16,8 @@ internal static class SegmentGenerator
     private static int _segmentsCount;
     private static int _lastSegmentSize;
     private static long _segmentSizeInBytes;
+
+    private static readonly Stopwatch Sw = new();
 
     private static int _calculationThreadCount = 0;
     
@@ -38,6 +41,9 @@ internal static class SegmentGenerator
     // TODO: рефакторинг
     private static void CheckQueueAndCalculateHashInBackgroundThreads()
     {
+        const int timeoutInSec = 5;
+        Sw.Start();
+        
         var backgroundThread = new Thread(() =>
         {
             while (true)
@@ -47,8 +53,12 @@ internal static class SegmentGenerator
                 if (_segmentTuples.IsEmpty)
                 {
                     Thread.Sleep(MyFavoriteNumber);
+                    if (Sw.Elapsed.Seconds >= timeoutInSec)
+                        return;
                     continue;
                 }
+                
+                Sw.Reset();
 
                 if (_calculationThreadCount < _necessaryCalculationThreadsCount)
                 {
@@ -60,9 +70,8 @@ internal static class SegmentGenerator
                         TryActionRelatesSegmentCalculation(() =>
                         {
                             var hash = SHA256.HashData(segmentTuple.Data);
-                            _calculationThreadCount++; // TODO: вынести вне тредов
+                            _calculationThreadCount++;
                             Console.WriteLine($"Сегмент #{segmentTuple.SegmentNumber}, хэш: {ToHexHashString(hash)}");
-                            //Console.WriteLine($"Tread count: {_calculationThreadCount}");
                         },
                             segmentTuple.SegmentNumber);
                     });
@@ -126,75 +135,7 @@ internal static class SegmentGenerator
         }
     }
 
-    private static void RunSegmentGenerationInThreads(FileStream fileStream,
-        List<Thread> threadList, long segmentSizeInBytes)
-    {
-        var fileSizeInBytes = fileStream.Length;
-
-        var optimalThreadsCount = Environment.ProcessorCount / 2;
-        var segmentsCount = fileSizeInBytes / segmentSizeInBytes;
-        var requiredThreadsCount = Math.Min(optimalThreadsCount, segmentsCount);
-        var segmentsPerThread = segmentsCount / requiredThreadsCount;
-        var segmentsRemnant = segmentsCount % requiredThreadsCount;
-
-        for (var i = 1; i <= requiredThreadsCount; i++)
-        {
-             var thread = new Thread(() => ComputeSha256(fileStream,
-                 (int)segmentSizeInBytes, segmentsPerThread, segmentsRemnant))
-             {
-                 Name = $"{i}"
-             };
-             threadList.Add(thread);
-             
-             thread.Start();
-        }
-    }
-
-    private static void ComputeSha256(FileStream fileStream, int bytesCount, long segmentsPerThread,
-        long segmentsRemnant)
-    {
-        lock (fileStream)
-        {
-            var currentThreadNumber = Thread.CurrentThread.Name!.ToInt();
-            var segmentsForCurrentThread = currentThreadNumber <= segmentsRemnant
-                ? segmentsPerThread + 1
-                : segmentsPerThread;
-
-            var firstSegmentNumber = currentThreadNumber <= segmentsRemnant
-                ? currentThreadNumber * segmentsForCurrentThread - segmentsPerThread
-                : currentThreadNumber * segmentsForCurrentThread + segmentsRemnant;
-
-            foreach (var additionNumber in Enumerable.Range(0, (int)segmentsForCurrentThread))
-            {
-                var currentSegmentNumber = firstSegmentNumber + additionNumber;
-
-                var buffer = new byte[bytesCount];
-
-                try
-                {
-                    fileStream.Position = (currentSegmentNumber - 1) * bytesCount;
-                    var readBytes = fileStream.Read(buffer, 0, bytesCount);
-                    if (readBytes <= 0) return;
-                    var hash = SHA256.HashData(buffer);
-                    Console.WriteLine($"Сегмент #{currentSegmentNumber}, хэш: {ToHexHashString(hash)}");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Расчет сегмента #{currentSegmentNumber} завершился с ошибкой: {e}");
-                    Console.WriteLine($"Stack trace: {e.StackTrace}");
-                }
-                finally
-                {
-                    Console.WriteLine();
-                }
-            }
-        }
-    }
-
-    
-    // TODO: проверить что будет, если передавть сюда полный массив, либо 2 по частям. Если строка хэша сходится,
-    // то можно органиваться систему. не падающую при большом введенном сегменте
-    private static string ToHexHashString(byte[] hash256)
+   private static string ToHexHashString(byte[] hash256)
     {
         var sb = new StringBuilder();
         foreach (var sigh in hash256)
